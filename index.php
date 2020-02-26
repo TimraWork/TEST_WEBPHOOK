4cfb61e25716050ff1028438a5563b8873796420
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-02-26 13:41:07 +0500

    Edit index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-hello_world
+<?php echo 'lorem impsum'; ?>
+<?php echo 'lorem impsum'; ?>
+<?php echo 'lorem impsum'; ?>
+<?php echo 'lorem impsum'; ?>
+<?php echo 'lorem impsum'; ?>
+<?php echo 'lorem impsum'; ?>
+<?php echo 'lorem impsum'; ?>
